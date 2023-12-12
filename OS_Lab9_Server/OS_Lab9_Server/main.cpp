d77c9f96297e0ff57db8da94ac60b9c3e157c56f
PavloStr111 <pavlostrep@gmail.com>
2023-12-12 21:45:40 +0200

    Server released by Pavlo

--- OS_Lab9_Server/OS_Lab9_Server/main.cpp
+++ OS_Lab9_Server/OS_Lab9_Server/main.cpp
@@ -1,19 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <string>
 #include <cstring>
 #include <vector>
 
 const int MaxClients = 100;
-const int MaxMessageLength = 200;
-const int MaxBadWord = 3;
-char*** badWord;
+const int MaxMessageLength = 260;
+int MaxBadWord = 0;
+std::vector<std::string> badWord;
+int** g_d;
 CRITICAL_SECTION criticalSection;
+HANDLE semaphore;
 
-bool correct[100000][MaxBadWord];
-bool messagesChecked[100000][MaxBadWord];
+bool** correct;
+bool** messagesChecked;
 
 //------------------------------ "User data for current moment"
 struct UserData {
@@ -26,6 +29,7 @@ struct UserData {
 };
 
 struct UsersLog {
+
     UserData user[MaxClients];
 };
 //------------------------------
@@ -41,28 +45,116 @@ struct MessagesLog {
 
 MessagesLog* messages;
 UsersLog* users;
+void prefixFunction(std::string key, int* d) {
+    int currJointSize = 0; // довжина поточного співпадіння префікса та суфікса
+    
 
-DWORD findBadWord(LPVOID param)
+    int i = 1; // d[0] = 0
+    while (i < key.size()) {
+        if (key[i] == key[currJointSize]) {
+            currJointSize++;
+            d[i] = currJointSize;
+            i++;
+        }
+        else {
+            if (currJointSize != 0) {
+                currJointSize = d[currJointSize - 1];
+            }
+            else {
+                d[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+
+
+int searchWord(std::string text, std::string word, int* d) {
+    int textIndex = 0; // Індекс для тексту
+    int wordIndex = 0; // Індекс для слова
+    int numOfBadWords = 0;
+    //std::cout << "In search";
+    while (textIndex < text.size()) {
+        if (word[wordIndex] == text[textIndex]) {
+            textIndex++;
+            wordIndex++;
+        }
+        if (wordIndex == word.size()) {
+            numOfBadWords++;
+            wordIndex = d[wordIndex - 1]; // Використовуємо префікс-функцію для зміщення слова
+        }
+        else if (textIndex < text.size() && word[wordIndex] != text[textIndex]) {
+            if (wordIndex != 0) { // Використовуємо префікс-функцію для зміщення слова
+                textIndex -= d[wordIndex - 1];
+                wordIndex = 0;
+            }
+            else {
+                textIndex++;
+            }
+        }
+    }
+    return numOfBadWords;
+}
+
+void readBadWord(std::string filePath)
 {
+    std::ifstream inputFile(filePath);
+
+    // Перевірка, чи файл вдалося відкрити
+    if (!inputFile.is_open()) {
+        std::cerr << "Unable to open the file\n";
+        return; // Повертаємо код помилки
+    }
+
+    // Зчитування слів з файлу та додавання їх до масиву
+    std::string word;
+    while (inputFile >> word) {
+        badWord.push_back(word);
+        MaxBadWord++;
+    }
+
+    
+
+    // Закриття файлу
+    inputFile.close();
+
+    // Виведення слів з масиву (для перевірки)
+    std::cout << "Bad Words:\n";
+    for (const auto& badWord : badWord) {
+        std::cout << badWord << "\n";
+    }
+
+
+
+
+    return;
+}
+
+DWORD findBadWord(LPVOID param) {
     int* data = reinterpret_cast<int*>(param);
     int numOfModer = data[0], numOfMes = data[1];
 
     std::string m(messages->message[numOfMes]);
-    std::string bm(badWord[numOfMes][numOfModer]);
+    std::string bm(badWord[numOfModer]);
 
-    size_t found = m.find(bm);
-
-    if (found != std::string::npos) {
-        correct[numOfMes][numOfModer] = true;
+    // Знаходимо позицію першої двокрапки
+    size_t colonPos = m.find(':');
+    // Перевіряємо, чи знайдено двокрапку
+    if (colonPos != std::string::npos) {
+        // Якщо знайдено, починаємо пошук від позиції двокрапки
+        m = m.substr(colonPos);
     }
 
-    std::cout << "Message was checked" << numOfModer << '\n';
+    correct[numOfMes][numOfModer] = searchWord(m, bm, g_d[numOfModer]);
 
-    /*if (std::strstr(messages->message[numOfMes], badWord[numOfMes][numOfModer]) != nullptr) {
-        correct[numOfMes][numOfModer] = true;
-    }*/
+   
+    std::cout << "Message was checked by " << numOfModer << ". Num of bad words: " << correct[numOfMes][numOfModer] << std::endl;
+
 
     messagesChecked[numOfMes][numOfModer] = true;
+
+    delete[] data;
 
     return 0;
 }
@@ -72,7 +164,9 @@ DWORD findBadWord(LPVOID param)
 DWORD checkMessage(LPVOID param)
 {
     int id = reinterpret_cast<int>(param);
-    int data1[2], data2[2], data3[2];
+    //int data1[2], data2[2], data3[2];
+    int* data;
+    int messNum = 0;
     int sum = 0;
 
     EnterCriticalSection(&criticalSection);
@@ -81,45 +175,171 @@ DWORD checkMessage(LPVOID param)
         if (messages->message[i][0] == '\0' &&
             messages->message[i][1] == '\0')
         {
-            strcpy_s(messages->message[i], 200, users->user[id].messages);
-            data1[1] = i;
-            data2[1] = i;
-            data3[1] = i;
-            std::cout << "Message was add to log\n";
+            //std::cout << "before copy";
+            strcpy_s(messages->message[i], MaxMessageLength, users->user[id].messages);
+           
+            //std::cout << "after copy";
+            std::cout << "Message was added to log\n";
+
+            messNum = i;
+            for (int j = 0; j < MaxBadWord; j++)
+            {
+                //std::cout << "befor create thread";
+                data = new int[2];
+                data[0] = j;
+                data[1] = i;
+                CreateThread(NULL, 0, findBadWord, reinterpret_cast<LPVOID>(data), 0, NULL);
+                //std::cout << "after creat thread";
+            }
+
             break;
         }
     }
     LeaveCriticalSection(&criticalSection);
 
-    data1[0] = 0;
-    data2[0] = 1;
-    data3[0] = 2;
 
-
-    CreateThread(NULL, 0, findBadWord, reinterpret_cast<LPVOID>(data1), 0, NULL);
-    CreateThread(NULL, 0, findBadWord, reinterpret_cast<LPVOID>(data2), 0, NULL);
-    CreateThread(NULL, 0, findBadWord, reinterpret_cast<LPVOID>(data3), 0, NULL);
-
-    while (messagesChecked[data1[1]][0] != true && messagesChecked[data2[1]][1] != true && messagesChecked[data3[1]][2] != true)
+    
+    for(int i = 0; i < MaxBadWord; i++)
     {
+        if (messagesChecked[messNum][i] == true)
+        {
+            sum++;
+        }
+
+        if (sum == MaxBadWord)
+        {
+            sum = 0;
+            break;
+        }
+        else
+        {
+            i = 0;
+        }
     }
 
-    sum = (int)(correct[data1[1]][0]) + (int)(correct[data2[1]][2]) + (int)(correct[data3[1]][2]);
-
-    if (sum > 2)
+  
+    for (int i = 0; i < MaxBadWord; i++)
     {
-        messages->correct[data1[1]] = false;
-        std::cout << "uncorrect\n";
+        sum += (int)(correct[messNum][i]);
+    }
+
+ 
+
+    if (sum >= 2)
+    {
+        messages->correct[messNum] = false;
+        std::cout << "Incorrect message\n";
     }
     else
     {
-        messages->correct[data1[1]] = true;
-        std::cout << "correct\n";
+        messages->correct[messNum] = true;
+        std::cout << "Correct message\n";
     }
 
-    messages->canBeRead[data1[1]] = true;
-
+    messages->canBeRead[messNum] = true;
+   
     return 0;
+}
+
+void saveUserInCatalog(UserData &user) {
+    HANDLE hFile = CreateFileA(
+        "UserCatalog.txt",                          // Ім'я файлу
+        FILE_APPEND_DATA,                           // Режим: додавання до файлу
+        FILE_SHARE_WRITE | FILE_SHARE_READ,         // Дозвіл на спільний доступ
+        NULL,                                       // За замовчуванням
+        OPEN_ALWAYS,                                // Відкриваємо файл, якщо існує
+        FILE_ATTRIBUTE_NORMAL,                      // Звичайний файл
+        NULL                                        // Шаблон для файлу
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cerr << "Unable to open UserCatalog.txt for writing!" << std::endl;
+        return;
+    }
+
+    DWORD bytesWritten;
+    std::string userData;
+    userData = std::string(user.nickname) + " : " + std::string(user.password) + "\n";
+
+    WriteFile(
+        hFile,                              // Дескриптор файлу
+        userData.c_str(),                   // Дані для запису
+        userData.length(),                  // Розмір даних
+        &bytesWritten,                      // Кількість записаних байтів
+        NULL                                // За замовчуванням
+    );
+
+    CloseHandle(hFile);
+}
+void loadUserCatalog(UsersLog** users) {
+    HANDLE hFile = CreateFileA(
+        "UserCatalog.txt",                          // Ім'я файлу
+        GENERIC_READ,                               // Режим: читання
+        FILE_SHARE_READ | FILE_SHARE_WRITE,         // Дозвіл на спільний доступ
+        NULL,                                       // За замовчуванням
+        OPEN_EXISTING,                              // Відкриваємо тільки існуючий файл
+        FILE_ATTRIBUTE_NORMAL,                      // Звичайний файл
+        NULL                                        // Шаблон для файлу
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cerr << "Unable to open UserCatalog.txt for reading!" << std::endl;
+        return;
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) {
+        std::cerr << "Error getting file size!" << std::endl;
+        CloseHandle(hFile);
+        return;
+    }
+
+    char* buffer = new char[fileSize + 1];
+    DWORD bytesRead;
+
+    if (!ReadFile(hFile, buffer, fileSize, &bytesRead, NULL)) {
+        std::cerr << "Error reading from file!" << std::endl;
+        CloseHandle(hFile);
+        delete[] buffer;
+        return;
+    }
+
+    buffer[bytesRead] = '\0'; // Додаємо завершуючий нуль для забезпечення коректного рядка
+
+    CloseHandle(hFile);
+
+    std::string data(buffer);
+    delete[] buffer;
+
+    size_t startPos = 0;
+    size_t endPos;
+    int currEmptyUserIndex = 0;
+    while ((endPos = data.find("\n", startPos)) != std::string::npos) {
+        std::string line = data.substr(startPos, endPos - startPos);
+        size_t delimiterPos = line.find(" : ");
+
+        if (delimiterPos != std::string::npos) {
+            std::string nickname = line.substr(0, delimiterPos);
+            std::string password = line.substr(delimiterPos + 3);
+
+            // Знаходження першого вільного місця для зберігання користувача
+            int freeIndex = -1;
+            while (currEmptyUserIndex < MaxClients) {
+                if ((*users)->user[currEmptyUserIndex].nickname[0] == '\0' && (*users)->user[currEmptyUserIndex].password[0] == '\0') {
+                    freeIndex = currEmptyUserIndex;
+                    break;
+                }
+                currEmptyUserIndex++;
+            }
+
+            if (freeIndex != -1) {
+                strcpy_s((*users)->user[freeIndex].nickname, MaxMessageLength, nickname.c_str());
+                strcpy_s((*users)->user[freeIndex].password, MaxMessageLength, password.c_str());
+            }
+        }
+
+        startPos = endPos + 1;
+    }
 }
 
 
@@ -177,7 +397,6 @@ int main() {
         std::cerr << "Mapping file was not created (log of users): " << GetLastError() << std::endl;
         return 1;
     }
-
     users = (UsersLog*)MapViewOfFile(
         fileUsersLog,
         FILE_MAP_ALL_ACCESS,
@@ -190,17 +409,11 @@ int main() {
         CloseHandle(users);
         return 1;
     }
-
     //________________________________________________________
 
+    
     for (int i = 0; i < 100000; i++)
     {
-        for (int j = 0; j < MaxBadWord; j++)
-        {
-            correct[i][j] = 0;
-
-
-        }
         messages->correct[i] = 0;
         messages->canBeRead[i] = 0;
         messages->message[i][0] = '\0';
@@ -209,71 +422,84 @@ int main() {
 
     for (int i = 0; i < MaxClients; ++i) {
         users->user[i].id = i;
+        users->user[i].inUse = false;
         users->user[i].signal = false;
         users->user[i].nickname[0] = '\0';
         users->user[i].password[0] = '\0';
 
     }
 
-    badWord = new char** [100000];
+    loadUserCatalog(&users); // завантаження записів про користувачів із файлу
+    readBadWord(std::string("BadWordList.txt"));
+ 
+    g_d = new int* [MaxBadWord]; // змінна для префікс-функції
+    for (int i = 0; i < MaxBadWord; i++) {
+        g_d[i] = new int[20];
+        for (int j = 0; j < 20; j++)
+        {
+            g_d[i][j] = 0;
+        }
+    }
+
+    correct = new bool* [100000];
+    messagesChecked = new bool* [100000];
 
     for (int i = 0; i < 100000; i++)
     {
-        badWord[i] = new char* [3];
-
-        badWord[i][0] = new char[10];
-        strcpy_s(badWord[i][0], 5, "here");
-
-        badWord[i][1] = new char[10];
-        strcpy_s(badWord[i][1], 5, "up");
-
-        badWord[i][2] = new char[10];
-        strcpy_s(badWord[i][2], 5, "gg");
-
-
-
-
+        correct[i] = new bool[MaxBadWord];
+        messagesChecked[i] = new bool[MaxBadWord];
     }
 
+    for (int i = 0; i < 100000; i++)
+    {
+        for (int j = 0; j < MaxBadWord; j++)
+        {
+            correct[i][j] = false;
+            messagesChecked[i][j] = false;
+        }
+    }
+
+    for (int i = 0; i < MaxBadWord; i++) {
+        //std::string key = badWord[i];
+        prefixFunction(badWord[i], g_d[i]);
+    }
     InitializeCriticalSection(&criticalSection);
+    
 
-    /* for (int i = 0; i < 100000; i++)
-     {
-         for (int j = 0; j < MaxBadWord; j++)
-         {
-             events[i][j] = CreateEvent(NULL, TRUE, FALSE, (L"SharedMemoryEvent" + std::to_wstring(i) + std::to_wstring(j)).c_str());
-         }
-     }*/
-
-
-     //________________________________________________________
+    //________________________________________________________
 
     std::string bufstr;
-    std::cout << "Input topic (200 letters): ";
+    std::cout << "Input topic (letters): ";
     std::getline(std::cin, bufstr);
     for (int i = 0; i < MaxMessageLength; i++)
     {
         messages->message[0][i] = bufstr.c_str()[i];
     }
-    messages->message[0][199] = '\0';
+    messages->message[0][MaxMessageLength-1] = '\0';
 
     std::cout << "SERVER IS READY" << std::endl;
 
 
 
 
-
+    
 
     while (true) {
         for (int i = 0; i < MaxClients; ++i) {
-
+            
 
             if (users->user[i].signal == true) {
-                std::cout << "New message from client" << i << ": " << users->user[i].messages << std::endl;
+                std::cout << "New message from client " << users->user[i].messages << std::endl;
                 CreateThread(NULL, 0, checkMessage, reinterpret_cast<LPVOID>(i), 0, NULL);
 
 
                 users->user[i].signal = false;
+            }
+            if (users->user[i].inUse == true)
+            {
+                // write in file.txt
+                saveUserInCatalog(users->user[i]);
+                users->user[i].inUse = false;
             }
         }
 
@@ -286,7 +512,7 @@ int main() {
 
 
 
-
+    
 
     UnmapViewOfFile(users);
     CloseHandle(fileUsersLog);
